ba68ba72b29fc362fcc4c4a050c2b30e0edb7e4d
Henrique Caetano <hecaetano1@hotmail.com>
2024-02-20 09:28:23 -0300

    feat: adiciona anotação equalsAndHashCode

--- linketinder/src/com/backend/linkeTinder/Candidato.groovy
+++ linketinder/src/com/backend/linkeTinder/Candidato.groovy
@@ -1,11 +1,12 @@
 package com.backend.linkeTinder
 
+import groovy.transform.EqualsAndHashCode
 import groovy.transform.ToString
 
 @ToString(includePackage = false, includeSuperProperties = true, includeNames = true)
+@EqualsAndHashCode
 class Candidato extends Usuario {
 
     String cpf;
     int idade;
-
 }
--- linketinder/src/com/backend/linkeTinder/Empresa.groovy
+++ linketinder/src/com/backend/linkeTinder/Empresa.groovy
@@ -1,9 +1,10 @@
 package com.backend.linkeTinder
 
+import groovy.transform.EqualsAndHashCode
 import groovy.transform.ToString
 
 @ToString(includePackage = false, includeSuperProperties = true, includeNames = true)
+@EqualsAndHashCode
 class Empresa extends Usuario {
-
     String cnpj, pais;
 }
