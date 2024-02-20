751bee035c9af5bc167a70d883eada0a486a391d
Henrique Caetano <hecaetano1@hotmail.com>
2024-02-20 09:18:46 -0300

    feat: refatora métodos de construção de objetos

--- linketinder/src/com/backend/linkeTinder/InputServices.groovy
+++ linketinder/src/com/backend/linkeTinder/InputServices.groovy
@@ -1,35 +1,36 @@
 package com.backend.linkeTinder
 
-class InputServices {
+class InputServices{
     static LinkedList<Candidato> obtemCandidatos(File arquivoCandidatos) {
         LinkedList<Candidato> candidatos = new LinkedList<>();
         Scanner leitura = new Scanner(arquivoCandidatos);
-
         int numCandidatos = leitura.nextLine() as Integer;
-        leitura.nextLine();
 
-        for (int i = 0; i < numCandidatos; i++) {
-
-            String nomeAtual = leitura.nextLine();
-            String emailAtual = leitura.nextLine();
-            String cpfAtual = leitura.nextLine();
-            int idadeAtual = leitura.nextLine() as Integer;
-            String estadoAtual = leitura.nextLine();
-            String cepAtual = leitura.nextLine();
-            String descricaoAtual = leitura.nextLine();
-
-            LinkedList<String> competenciasAtuais = new LinkedList<>();
-            String[] competenciasSeparadas = leitura.nextLine().split(",")
-            competenciasSeparadas.each { competenciasAtuais.add(it) }
-
-            leitura.nextLine()
-
-            Candidato candidatoAtual = new Candidato(nome: nomeAtual, email: emailAtual, cpf: cpfAtual, idade: idadeAtual, estado: estadoAtual, cep: cepAtual, descricao: descricaoAtual, competencias: competenciasAtuais);
+        for(i in 0..<numCandidatos){
+            leitura.nextLine();
+            Candidato candidatoAtual = constroiCandidatoViaArquivo(leitura);
             candidatos.add(candidatoAtual);
         }
         return candidatos;
+    }
+
+    static Candidato constroiCandidatoViaArquivo(Scanner leitura) {
+
+        String nomeAtual = leitura.nextLine();
+        String emailAtual = leitura.nextLine();
+        String cpfAtual = leitura.nextLine();
+        int idadeAtual = leitura.nextLine() as Integer;
+        String estadoAtual = leitura.nextLine();
+        String cepAtual = leitura.nextLine();
+        String descricaoAtual = leitura.nextLine();
+
+        LinkedList<String> competenciasAtuais = new LinkedList<>();
+        String[] competenciasSeparadas = leitura.nextLine().split(",")
+        competenciasSeparadas.each { competenciasAtuais.add(it) }
 
 
+        Candidato candidato = new Candidato(nome: nomeAtual, email: emailAtual, cpf: cpfAtual, idade: idadeAtual, estado: estadoAtual, cep: cepAtual, descricao: descricaoAtual, competencias: competenciasAtuais);
+        return candidato;
     }
 
     static LinkedList<Empresa> obtemEmpresas(File arquivoEmpresas) {
@@ -37,28 +38,38 @@ class InputServices {
         Scanner leitura = new Scanner(arquivoEmpresas);
 
         int numEmpresas = leitura.nextLine() as Integer;
-        leitura.nextLine();
 
         for (i in 0..<numEmpresas) {
-
-            String nomeAtual = leitura.nextLine();
-            String emailAtual = leitura.nextLine();
-            String cnpjAtual = leitura.nextLine();
-            String paisAtual = leitura.nextLine();
-            String estadoAtual = leitura.nextLine();
-            String cepAtual = leitura.nextLine();
-            String descricaoAtual = leitura.nextLine();
-
-            LinkedList<String> competenciasAtuais = new LinkedList<>();
-            String[] competenciasSeparadas = leitura.nextLine().split(",")
-            competenciasSeparadas.each { competenciasAtuais.add(it) }
-
-            leitura.nextLine()
-
-            Empresa empresaAtual = new Empresa(nome: nomeAtual, email: emailAtual, cnpj: cnpjAtual, pais: paisAtual, estado: estadoAtual, cep: cepAtual, descricao: descricaoAtual, competencias: competenciasAtuais);
+            leitura.nextLine();
+            Empresa empresaAtual = constroiEmpresaViaArquivo(leitura)
             empresas.add(empresaAtual);
         }
         return empresas;
-
     }
+
+    public static Empresa constroiEmpresaViaArquivo(Scanner leitura) {
+
+        String nomeAtual = leitura.nextLine();
+        String emailAtual = leitura.nextLine();
+        String cnpjAtual = leitura.nextLine();
+        String paisAtual = leitura.nextLine();
+        String estadoAtual = leitura.nextLine();
+        String cepAtual = leitura.nextLine();
+        String descricaoAtual = leitura.nextLine();
+
+        LinkedList<String> competenciasAtuais = new LinkedList<>();
+        String[] competenciasSeparadas = leitura.nextLine().split(",")
+        competenciasSeparadas.each { competenciasAtuais.add(it) }
+
+        Empresa empresa = new Empresa(nome: nomeAtual, email: emailAtual, cnpj: cnpjAtual, pais: paisAtual, estado: estadoAtual, cep: cepAtual, descricao: descricaoAtual, competencias: competenciasAtuais);
+        return empresa;
+    }
+
+//    static LinkedList<Candidato> cadastraNovoCandidato(LinkedList<Candidato>, Candidato candidato){
+//
+//    }
+//
+//    static LinkedList<Empresa> cadastraNovaEmpresa(LinkedList<Empresa>, Empresa empresa){
+//
+//    }
 }
