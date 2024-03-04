2b8ad5214e621e18ac01c523d581a8bfcc984697
Henrique Caetano <hecaetano1@hotmail.com>
2024-03-04 10:37:06 -0300

    feat: implementação da classe candidato

---
+++ linketinder/frontend/scripts/Candidato.ts
@@ -0,0 +1,21 @@
+import {Usuario} from "./Usuario";
+
+export class Candidato extends Usuario{
+    private _cpf: string;
+    private _idade: number;
+
+
+    constructor(nome: string, email: string, estado: string, cep: string, descricao: string, competencias: string[], cpf: string, idade: number) {
+        super(nome, email, estado, cep, descricao, competencias);
+        this._cpf = cpf;
+        this._idade = idade;
+    }
+
+    get cpf(): string {
+        return this._cpf;
+    }
+
+    get idade(): number {
+        return this._idade;
+    }
+}
