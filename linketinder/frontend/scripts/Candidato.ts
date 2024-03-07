111c03e18bfc7aa4fb8f19c98bcd76a6833dfd56
Henrique Caetano <hecaetano1@hotmail.com>
2024-03-07 17:18:10 -0300

    feat: adiciona atributo estado

--- linketinder/frontend/scripts/Candidato.ts
+++ linketinder/frontend/scripts/Candidato.ts
@@ -3,12 +3,14 @@ import {Usuario} from "./Usuario.js";
 export class Candidato extends Usuario{
     private _cpf: string;
     private _idade: number;
+    private _estado: string;
 
 
     constructor(nome: string, email: string, senha: string, estado: string, cep: string, descricao: string, competencias: string[], cpf: string, idade: number) {
-        super(nome, email, senha, estado, cep, descricao, competencias);
+        super(nome, email, senha, cep, descricao, competencias);
         this._cpf = cpf;
         this._idade = idade;
+        this._estado = estado;
     }
 
 
@@ -18,5 +20,9 @@ export class Candidato extends Usuario{
 
     get idade(): number {
         return this._idade;
+    }
+
+    get estado(): string {
+        return this._estado;
     }
 }
