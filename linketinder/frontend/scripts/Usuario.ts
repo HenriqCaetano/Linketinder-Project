97c91e3f595756a0c72f0e9b03d673496c7d09d2
Henrique Caetano <hecaetano1@hotmail.com>
2024-03-07 17:16:33 -0300

    fix: retira atributo estado

--- linketinder/frontend/scripts/Usuario.ts
+++ linketinder/frontend/scripts/Usuario.ts
@@ -2,17 +2,16 @@ export class Usuario {
     private _nome: string;
     private _email: string;
     private _senha: string;
-    private _estado: string;
     private _cep: string;
     private _descricao: string;
     private _competencias: string[];
 
 
-    constructor(nome: string, email: string, senha: string, estado: string, cep: string, descricao: string, competencias: string[]) {
+    constructor(nome: string, email: string, senha: string, cep: string, descricao: string, competencias: string[]) {
         this._nome = nome;
         this._email = email;
         this._senha = senha;
-        this._estado = estado;
+
         this._cep = cep;
         this._descricao = descricao;
         this._competencias = competencias;
@@ -29,10 +28,6 @@ export class Usuario {
 
     get email(): string {
         return this._email;
-    }
-
-    get estado(): string {
-        return this._estado;
     }
 
     get cep(): string {
