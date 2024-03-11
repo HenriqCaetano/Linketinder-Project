2f09b63b61822b0b6ae47743760361535d833f92
Henrique Caetano <hecaetano1@hotmail.com>
2024-03-11 17:27:29 -0300

    feat: implementa visualização de informações de candidatos

--- linketinder/frontend/scripts/scriptPerfilEmpresa.ts
+++ linketinder/frontend/scripts/scriptPerfilEmpresa.ts
@@ -1,11 +1,17 @@
 import {Candidato} from "./Candidato.js";
-import {Usuario} from "./Usuario.js";
-// import Chart from 'chart.js/auto';
 
 
 
 const displayCandidatos : HTMLElement = document.getElementById("lista-candidatos");
-const listaCandidatos :Candidato[]= JSON.parse(localStorage.getItem("listaCandidatos"))
+const listaInformacoesCandidatos:any[]= await JSON.parse(localStorage.getItem("listaCandidatos")); //chegam objetos sem tipo
+const listaCandidatos:Candidato[] = []; //lista para guardar os candidatos
+
+
+//reconstroi os candidatos a partir do localStorage
+listaInformacoesCandidatos.forEach(entry =>{
+    let novoCandidato:Candidato = new Candidato(entry._nome, entry._email,entry._senha, entry._estado, entry._cep, entry._descricao, entry._competencias, entry._cpf, entry._idade);
+    listaCandidatos.push(novoCandidato)
+})
 
 
 //construção do objeto do gráfico
@@ -17,27 +23,41 @@ type entrada = {
 let dadosGrafico: entrada[] = []
 
 
-console.log(listaCandidatos)
 listaCandidatos.forEach((candidato:Candidato) => {
-    console.log(candidato.competencias)
 
-    // candidato.competencias.forEach((competencia:string) => {
-    //     let competenciaDeveSerAdicionada = true;
-    //     for (let i = 0; i < dadosGrafico.length; i++) {
-    //         if(dadosGrafico[i].competencia === competencia){
-    //             dadosGrafico[i].quantidade++;
-    //             competenciaDeveSerAdicionada = false;
-    //             break;
-    //         }
-    //     }
-    //     if(competenciaDeveSerAdicionada){
-    //         let novaEntrada:entrada = {competencia: competencia, quantidade:1};
-    //         dadosGrafico.push(novaEntrada);
-    //     }
-    // })
+    candidato.competencias.forEach((competencia:string) => {
+        let competenciaDeveSerAdicionada = true;
+        for (let i = 0; i < dadosGrafico.length; i++) {
+            if(dadosGrafico[i].competencia === competencia){
+                dadosGrafico[i].quantidade++;
+                competenciaDeveSerAdicionada = false;
+                break;
+            }
+        }
+        if(competenciaDeveSerAdicionada){
+            let novaEntrada:entrada = {competencia: competencia, quantidade:1};
+            dadosGrafico.push(novaEntrada);
+        }
+    })
 
 })
 console.log(dadosGrafico)
+
+
+// @ts-ignore
+new Chart(document.getElementById("grafico-competencias"),
+    {
+        type: 'bar',
+        data: {
+            labels: dadosGrafico.map(row => row.competencia),
+            datasets: [
+                {
+                    label: 'Quantia de candidatos por competência',
+                    data: dadosGrafico.map(row => row.quantidade)
+                }
+            ]
+        }
+    })
 
 listaCandidatos.forEach((candidato : Candidato) => {
     displayCandidatos.innerHTML += `<li>Descrição: ${candidato.descricao}, Competências: ${candidato.competencias} <li>`
