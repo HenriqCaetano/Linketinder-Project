e693708eca4cf5c3b134147c92b1cc589913bbf9
Henrique Caetano <hecaetano1@hotmail.com>
2024-03-11 17:31:31 -0300

    fix: conserta obtenção de empresas via local storage

--- linketinder/frontend/scripts/scriptPerfilCandidato.ts
+++ linketinder/frontend/scripts/scriptPerfilCandidato.ts
@@ -1,8 +1,18 @@
 import {Empresa} from "./Empresa";
+import {Candidato} from "./Candidato";
 
 
 const displayVagas = document.getElementById("lista-empresas");
-const listaEmpresas: Empresa[] = JSON.parse(localStorage.getItem("listaEmpresas"));
+
+const listaInformacoesEmpresas:any[]= await JSON.parse(localStorage.getItem("listaEmpresas")); //chegam objetos sem tipo
+const listaEmpresas:Empresa[] = []; //lista para guardar as empresas
+
+
+//reconstroi as empresas a partir do localStorage
+listaInformacoesEmpresas.forEach(entry =>{
+    let novaEmpresa:Empresa = new Empresa(entry._nome,entry._email, entry._senha, entry._cep, entry._descricao, entry._competencias, entry._cnpj, entry._pais);
+    listaEmpresas.push(novaEmpresa)
+})
 
 
 for (const empresa of listaEmpresas) {
